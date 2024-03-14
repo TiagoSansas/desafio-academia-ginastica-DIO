99c1ad33c2298ac2f0ad010f3bc629d9be340b19
Tiago Souza <tiagoeduardosouza@gmail.com>
2024-03-14 19:53:00 -0300

    Inciando projeto Academia

---
+++ src/test/java/com/tiagosouza/academiaDio/AcademiaDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagosouza.academiaDio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
